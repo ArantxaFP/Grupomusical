2af734f6db97a5a55202d53fc6cd0742b9336d14
ArantxaFP <afernandezpere@uoc.edu>
2024-05-27 18:21:45 +0200

    prueba rutas index

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,20 +1,63 @@
-// Obtener la URL de la página actual
-var currentUrl = window.location.href;
+document.addEventListener('DOMContentLoaded', () => {
 
-// Obtener el menú de navegación
-var menu = document.getElementById('main-menu');
+  // Función para mostrar menú en mobile
+  const menuToggle = document.querySelector('.menu-toggle');
+  const closeButton = document.querySelector('.close-btn');
+  const navUl = document.querySelector('header nav ul');
 
-// Obtener todos los enlaces dentro del menú
-var links = menu.getElementsByTagName('a');
+  menuToggle.addEventListener('click', () => {
+    navUl.classList.toggle('is-visible');
+    closeButton.style.display = navUl.classList.contains('is-visible') ? 'block' : 'none'; // Mostrar/ocultar botón de cierre
+  });
 
-// Recorrer los enlaces
-for (var i = 0; i < links.length; i++) {
-  // Obtener la URL de cada enlace
-  var linkUrl = links[i].href;
+  closeButton.addEventListener('click', () => {
+    navUl.classList.remove('is-visible');
+    closeButton.style.display = 'none'; // Oculta el botón de cierre
+  });
 
-  // Comparar la URL de cada enlace con la URL de la página actual
-  if (currentUrl === linkUrl) {
-    // Si son iguales, añadir la clase 'current' al enlace
-    links[i].classList.add('current');
+
+
+  // Función para marcar el enlace activo tanto en el header como en el footer
+  function markCurrentLink(menuId) {
+    const menu = document.getElementById(menuId);
+    if (!menu) {
+      console.error(`El elemento con id "${menuId}" no se encontró en el DOM.`);
+      return;
+    }
+
+    const links = menu.getElementsByTagName('a');
+    const currentUrl = window.location.href;
+
+    for (let link of links) {
+      if (currentUrl === link.href) {
+        link.classList.add('current');
+      }
+    }
   }
-}
+
+  // Marca el enlace activo en el menú del header
+  markCurrentLink('main-menu');
+
+  // Marca el enlace activo en el menú del footer
+  markCurrentLink('footer-menu');
+
+
+
+  //botón subir
+  const btnScrollTop = document.getElementById("btn-scroll-top");
+
+  btnScrollTop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 500) {
+      btnScrollTop.classList.remove("hidden");
+    } else {
+      btnScrollTop.classList.add("hidden");
+    }
+  });
+});
